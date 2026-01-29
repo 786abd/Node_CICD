5b971e13598ece6681dcaef0f034c47b1d78f9ca
Abdullah Sheikh <abdullahsheikh9289@gmail.com>
2026-01-29 15:17:38 +0530

    Trying sonarqube in pipeline

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT ?? 80
 
 app.get('/', (req, res) => {
-    return res.json({msg: 'Lets eat the whole CICD'})
+    return res.json({msg: 'Now we checking every line of code as SQ arrives'})
 })
 
 app.listen(PORT, '0.0.0.0', () => {
